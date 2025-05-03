588f2059218b222a5a0b03e3bf4c498849ac4ed5
Luka Terzic <lukaterzicbg@gmail.com>
2025-05-03 23:18:33 +0200

    3d sensors now outputting vectors

--- include/emblib/driver/sensor/sensor3d.hpp
+++ include/emblib/driver/sensor/sensor3d.hpp
@@ -2,14 +2,17 @@
 
 #include "emblib/emblib.hpp"
 #include "sensor.hpp"
+#include "emblib/math/vector.hpp"
 
 namespace emblib::driver {
 
 template <typename axis_data_type>
-class sensor3d : public sensor<axis_data_type[3]> {
+class sensor3d : public sensor<vector<axis_data_type, 3>> {
 
 public:
-    /* Used for read axis method */
+    /**
+     * Axis enumerator for selecting when reading single axis
+     */
     enum class axis_e {
         X, Y, Z
     };
@@ -31,12 +34,12 @@ public:
      * @note Default implementation is to read all 3 axes one by one, but
      * can be overriden to allow reading of all the values in one bus transaction
      */
-    virtual bool read(axis_data_type (&out_data)[3]) override noexcept
+    virtual bool read(vector<axis_data_type, 3>& out) override noexcept
     {
         bool success = true;
-        success &= read_axis(axis_e::X, out_data[(int)axis_e::X]);
-        success &= read_axis(axis_e::Y, out_data[(int)axis_e::Y]);
-        success &= read_axis(axis_e::Z, out_data[(int)axis_e::Z]);
+        success &= read_axis(axis_e::X, out((int)axis_e::X));
+        success &= read_axis(axis_e::Y, out((int)axis_e::Y));
+        success &= read_axis(axis_e::Z, out((int)axis_e::Z));
         return success;
     }
 
