7e4cd6a9f73a0b72152f02fc743bc13d09b872ca
Luka Terzic <lukaterzicbg@gmail.com>
2025-05-23 20:41:55 +0200

    submodules fetched as needed

---
+++ lib/addsubmodule.cmake
@@ -0,0 +1,16 @@
+# Macro to init and add specific submodule
+#
+# Note: Using function instead of macro doesn't
+# work with Catch2
+macro(add_submodule path)
+    message(STATUS "Adding submodule: ${path}")
+    execute_process(COMMAND git submodule update --init --recursive ${path}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    RESULT_VARIABLE result
+                    OUTPUT_VARIABLE output
+                    ERROR_VARIABLE error)
+    add_subdirectory(${path})
+    if(NOT result EQUAL 0)
+        message(FATAL_ERROR "Failed to initialize submodule: ${path}\n${output}\n${error}")
+    endif()
+endmacro()
