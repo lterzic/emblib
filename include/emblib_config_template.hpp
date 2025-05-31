da50e44d11eecadfa087f32b30bde7dcfea44f93
Luka Terzic <lukaterzicbg@gmail.com>
2025-05-31 11:18:00 +0200

    impl related defines and cmake fixes

--- include/emblib_config_template.hpp
+++ include/emblib_config_template.hpp
@@ -3,11 +3,3 @@
 // Allows for using all emblib definitions from the emblib namespace
 // instead of nesting them (emblib::mutex instead of emblib::rtos::mutex, etc)
 #define EMBLIB_UNNEST_NAMESPACES    1
-
-// Select the RTOS backend
-#define EMBLIB_RTOS_USE_FREERTOS    1
-#define EMBLIB_RTOS_USE_THREADX     0
-
-// Select the math backend
-#define EMBLIB_MATH_USE_GLM         0
-#define EMBLIB_MATH_USE_EIGEN       1
